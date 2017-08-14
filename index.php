e598d86e379528bee29fd579b06435e8a6578fc0
Jeff Kranking <jkranking@gmail.com>
2017-08-14 11:09:56 -0700

    creates read and create features

--- index.php
+++ index.php
@@ -12,12 +12,16 @@
                 <h3>PHP CRUD Grid</h3>
             </div>
             <div class="row">
+                <p>
+                    <a href="create.php" class="btn btn-success">Create</a>
+                </p>
                 <table class="table table-striped table-bordered">
                   <thead>
                     <tr>
                       <th>Name</th>
                       <th>Email Address</th>
                       <th>Mobile Number</th>
+                      <th>Action</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -30,6 +34,7 @@
                             echo '<td>'. $row['name'] . '</td>';
                             echo '<td>'. $row['email'] . '</td>';
                             echo '<td>'. $row['mobile'] . '</td>';
+                            echo '<td><a class="btn" href="read.php?id='.$row['id'].'">Read</a></td>';
                             echo '</tr>';
                    }
                    Database::disconnect();
